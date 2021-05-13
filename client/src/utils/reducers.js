f6b26c7e7770d905a3e64e5cbe55cc4dd640482f
gwarz <gmwarzecha@tutanota.com>
2021-05-13 16:31:01 -0700

    added tests and corresponding reducers, verified failing and passing test results

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -2,8 +2,14 @@ import { useReducer } from 'react';
 
 import {
   UPDATE_PRODUCTS,
+  UPDATE_CATEGORIES,
   UPDATE_CURRENT_CATEGORY,
-  UPDATE_CATEGORIES
+  ADD_TO_CART,
+  ADD_MULTIPLE_TO_CART,
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  CLEAR_CART,
+  TOGGLE_CART
 } from './actions';
 
 export const reducer = (state, action) => {
@@ -14,23 +20,67 @@ export const reducer = (state, action) => {
       return {
         // return a new obj with a copy of the state arg and set products key to a value
         //of a new array with the action.products value spread across it
-        ...state, 
+        ...state,
         products: [...action.products],
       };
     case UPDATE_CATEGORIES:
       return {
-        ...state, 
+        ...state,
         categories: [...action.categories]
       };
     case UPDATE_CURRENT_CATEGORY:
       return {
+        //...state preserves everything else on state
         ...state,
         currentCategory: action.currentCategory
       };
+    case ADD_TO_CART:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: [...state.cart, action.product]
+      };
+    case ADD_MULTIPLE_TO_CART:
+      return {
+        ...state,
+        cart: [...state.cart, ...action.products],
+      };
+    case REMOVE_FROM_CART:
+      let newState = state.cart.filter(product => {
+        return product._id !== action._id;
+      });
 
-      // if it's none of these actions, do not update state at all and keep all the same
-      default:
-        return state;
+      return {
+        ...state,
+        cartOpen: newState.length > 0,
+        cart: newState
+      };
+    case UPDATE_CART_QUANTITY:
+      return {
+        ...state,
+        cartOpen: true,
+        // map is used because the original state should be treated as immutable
+        cart: state.cart.map(product => {
+          if (action._id === product._id) {
+            product.purchaseQuantity = action.purchaseQuantity;
+          }
+          return product;
+        })
+      };
+    case CLEAR_CART:
+      return {
+        ...state, 
+        cartOpen: false, 
+        cart: []
+      };
+    case TOGGLE_CART:
+      return {
+        ...state, 
+        cartOpen: !state.cartOpen
+      };
+    // if it's none of these actions, do not update state at all and keep all the same
+    default:
+      return state;
   }
 };
 
