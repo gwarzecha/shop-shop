accc1eb0a113b2866fe75c9ffafd9130eca6684b
gwarz <gmwarzecha@tutanota.com>
2021-05-13 19:55:26 -0700

    updated cart to handle direct values being passed in for quantity, and deleting items from cart

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -5,6 +5,8 @@ import { useStoreContext } from '../../utils/GlobalState';
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 
 function ProductItem(item) {
+  const [state, dispatch] = useStoreContext();
+
   const {
     image,
     name,
@@ -13,14 +15,27 @@ function ProductItem(item) {
     quantity
   } = item;
 
-  const [state, dispatch] = useStoreContext();
+const { cart } = state;
 
-  const addToCart = () => {
+const addToCart = () => {
+  // find the cart item with the matching id
+  const itemInCart = cart.find((cartItem) => cartItem._id === _id);
+
+  // if there was a match, call UPDATE with a new purchase quantity
+  if (itemInCart) {
+    dispatch({
+      type: UPDATE_CART_QUANTITY,
+      _id: _id,
+      purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+    });
+  } else {
     dispatch({
       type: ADD_TO_CART,
       product: { ...item, purchaseQuantity: 1 }
     });
-  };
+  }
+};
+
 
   return (
     <div className="card px-1 py-1">
