7758d3cd8caafad0d8cd8f03d18adf008d0ad6f2
gwarz <gmwarzecha@tutanota.com>
2021-05-13 19:32:40 -0700

    added ability to add items to cart, cart also shows empty when nothing is in it

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -3,7 +3,12 @@ import { Link, useParams } from "react-router-dom";
 import { useQuery } from '@apollo/react-hooks';
 
 import { useStoreContext } from '../utils/GlobalState';
-import { UPDATE_PRODUCTS } from '../utils/actions';
+import {
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  ADD_TO_CART,
+  UPDATE_PRODUCTS
+} from '../utils/actions';
 
 import { QUERY_PRODUCTS } from "../utils/queries";
 import spinner from '../assets/spinner.gif';
@@ -13,13 +18,20 @@ import Cart from '../components/Cart';
 function Detail() {
   const [state, dispatch] = useStoreContext();
   const { id } = useParams();
-  
+
   const [currentProduct, setCurrentProduct] = useState({})
-  
+
   const { loading, data } = useQuery(QUERY_PRODUCTS);
-  
+
   const { products } = state;
-  
+
+  const addToCart = () => {
+    dispatch({
+      type: ADD_TO_CART,
+      product: { ...currentProduct, purchaseQuantity: 1 }
+    });
+  };
+
   useEffect(() => {
     if (products.length) {
       setCurrentProduct(products.find(product => product._id === id));
@@ -49,12 +61,8 @@ function Detail() {
             <strong>Price:</strong>
             ${currentProduct.price}
             {" "}
-            <button>
-              Add to Cart
-            </button>
-            <button>
-              Remove from Cart
-            </button>
+            <button onClick={addToCart}>Add to cart</button>
+            <button>Remove from Cart</button>
           </p>
 
           <img
